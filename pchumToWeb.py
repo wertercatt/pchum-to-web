7a4bb059f3cebaed1e3b16dcfd8938ff4c1cd0e4
Blaze Marshall <wertercatt@wertercatt.com>
2022-12-13 16:59:32 -0600

    Get abbreviated chumhandles

--- pchumToWeb.py
+++ pchumToWeb.py
@@ -1,9 +1,12 @@
 import os
 import sys
+from bs4 import BeautifulSoup
+import json
+
 def convert(filePath):
     header = '<html><head>\n \
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">\n \
-    <title>{title} | Log for {year}-{month}-{day} starting at {hour}:{mins}</title>\n \
+ b   <title>{title} | Log for {year}-{month}-{day} starting at {hour}:{mins}</title>\n \
     <style>body {{ background-color: #101535; font-family: Courier New,Courier,monospace; font-weight: bold; }} h1{{ text-align: center; color: #FFFFFF; }} .memolog {{ text-align: left; background-color: #FFFFFF; border: 5px solid #3F4472; height: 500px; width: 48%; padding: 5px; margin-left: auto; overflow: scroll; margin-right: auto; float: center; }} .participants {{ text-align: left; background-color: #FFFFFF; border: 5px solid #3F4472; height: 500px; width: 22%; padding: 5px; margin-left: auto; overflow: scroll; margin-right: auto; float: right; white-space: nowrap }} .memolist {{ text-align: left; background-color: #FFFFFF; border: 5px solid #3F4472; height: 500px; width: 22%; padding: 5px; margin-left: auto; overflow: scroll; margin-right: auto; white-space: nowrap; float: left; }}</style>\n \
     <meta charset="UTF-8">\n \
     </head>\n \
@@ -20,9 +23,27 @@ def convert(filePath):
             "day": logName.split(".")[1].split("-")[2],
             "month": logName.split(".")[1].split("-")[1],
             "year": logName.split(".")[1].split("-")[0],
-            "title": logName.split(".")[0]
+            "title": logName.split(".")[0],
         }
         logHeader = header.format(**logInfo)
-        print(logHeader)
+        userInfo = []
+        unformattedLogParser = BeautifulSoup(unformattedLog.read(), 'html.parser')
+        messageSpans = unformattedLogParser.find_all("span")
+        for span in messageSpans:
+            spanText = span.get_text()
+            if len(spanText) > 3:
+                if spanText[3] == ":":
+                    chum = {
+                            "chum": spanText[1:3],
+                            "color": span["style"].split(":")[1],
+                            "tense": spanText[0],
+                        }
+                    if chum not in userInfo:
+                        userInfo.append(chum)
+        print(json.dumps(logHeader, sort_keys=True, indent=4))
+        print(json.dumps(userInfo, sort_keys=True, indent=4))
+
+
+
 if __name__ == "__main__":
     convert(sys.argv[1])
